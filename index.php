a2c186e1d360a7e8d8535edbedb0f8ea6bdaeb3c
Lightstorm1981 <36949851+Lightstorm1981@users.noreply.github.com>
2025-09-15 10:05:57 +0200

    index.php

--- index.php
+++ index.php
@@ -7,14 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
-// Nur guthaben-abfragen.php erlauben
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($path !== '/guthaben.php') {
-    http_response_code(404);
-    echo "Zugriff verweigert";
-    exit;
-}
-
 try {
     // Parameter sammeln
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
